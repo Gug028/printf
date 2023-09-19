5c3c68d100fd964ee114ecc5fbd19e954e8a77e4
Gugu <Gug028>
2023-09-19 07:27:28 -0700

    Collaboration update

--- main.h
+++ main.h
@@ -1,22 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#define UNUSED(x) (void)(x)
 #define BUFFER_SIZE 1024
-
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
 
 /**
  * struct fmt - Struct op
@@ -40,10 +29,5 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 void print_buffer(char buffer[], int *buff_ind);
-int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size); 
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
 
 #endif /* MAIN_H */
