102ac4ee9efabc268c2a17d685aab030454c732d
Gugu <Gug028>
2023-09-19 12:49:56 -0700

    Update

--- main.h
+++ main.h
@@ -1,28 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <unistd.h>
 
-#define UNUSED(x) (void)(x)
 #define BUFFER_SIZE 1024
-
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
 
 /**
  * struct fmt - Struct op
  *
- * @fmt: The format.
- * @fn: The function associated.
+ * @fmt: format.
+ * @fn: function associated
  */
 struct fmt
 {
@@ -32,14 +20,15 @@ struct fmt
 
 
 /**
- * typedef struct fmt fmt_t - Struct op
+ * typedef struct fmt fmt_t - struct op
  *
- * @fmt: The format.
- * @fm_t: The function associated.
+ * @fmt: format
+ * @fm_t: function associated
  */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[]);
+void print_buffer(char buffer[], int *buff_ind);
 
 #endif /* MAIN_H */
