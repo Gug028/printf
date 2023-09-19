bdaa953870c7f21cf227d9f0c849581b2d2767e7
Gugu <Gug028>
2023-09-19 07:37:02 -0700

    Collaboration Update

--- main.h
+++ main.h
@@ -7,26 +7,6 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- * struct fmt - Struct op
- *
- * @fmt: the format
- * @fn: the function associated
- */
-struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-};
-
-/**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: the format
- * @fm_t: the function associated
- */
-typedef struct fmt fmt_t;
-
 int _printf(const char *format, ...);
 void print_buffer(char buffer[], int *buff_ind);
 
