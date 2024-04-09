f4f0c23c82e75a0e9b77eeee9363b1c7a6da9709
Gug028 <gramthembu@gmail.com>
2024-04-09 12:54:12 -0700

    Printf corrections

--- _printf.c
+++ _printf.c
@@ -1,67 +1,169 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
-
-void print_buffer(char buffer[], int *buff_ind);
+#include "main.h"
+#include <stddef.h>
+#include <string.h>
 
 /**
- * _printf - Prints output
- * @format: pointer to string
- * Return: characters printed.
+ * check_str - Prints a string
+ * @str: The string to print
+ * @count: The number of characters printed
+ * @buffer: The buffer to store characters
+ * @buffer_index: The current index in the buffer
  */
-int _printf(const char *format, ...)
+void check_str(char *str, int *count, char *buffer, int *buffer_index)
 {
-	int print_char = 0;
-	int n, buff_ind = 0;
-	va_list mylist;
-	char buffer[BUFFER_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(mylist, format);
-
-	for (n = 0; format && format[n] != '\0'; n++)
+	if (!str)
+		str = "(null)";
+	
+	while (*str)
 	{
-		if (format[n] != '%')
-		{
-			buffer[buff_ind++] = format[n];
-			if (buff_ind == BUFFER_SIZE)
-			{
-				print_buffer(buffer, &buff_ind);
-			}
-
-			print_char++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			++n;
-
-			if (format[n] == '\0')
-				return (-1);
-			if (format[n] == '%')
-			{
-				buffer[buff_ind++] = format[n];
-				print_char++;
-			}
-		}
+		_putchar(*str, buffer, buffer_index);
+		str++;
+		(*count)++;
 	}
-	print_buffer(buffer, &buff_ind);
-	va_end(mylist);
-	return (print_char);
+
+}
+/**
+ * print_arg2 - A helper function to print args
+ * @format: A list of types of arguments passed to the function
+ * @args: The arguments to be printed
+ * @count: The number of characters printed
+ * @buffer: The buffer to store characters
+ * @buffer_index: The current index in the buffer
+ * @flag: contains the flags
+ */
+void print_arg2(const char *format, int *count, va_list args, char *buffer,
+				int *buffer_index, int flag)
+{
+	switch (*format)
+	{
+		case 'x':
+			print_hex(va_arg(args, unsigned int), 0, count, buffer, buffer_index, flag);
+			break;
+		case 'X':
+			print_hex(va_arg(args, unsigned int), 1, count, buffer, buffer_index, flag);
+			break;
+		case 'S':
+			print_str_nonChar(va_arg(args, char *), count, buffer, buffer_index);
+			break;
+		case 'p':
+			print_ptr(va_arg(args, void *), count, buffer, buffer_index);
+			break;
+		case 'o':
+			print_octal(va_arg(args, unsigned int), count, buffer, buffer_index, flag);
+			break;
+		default:
+			_putchar('%', buffer, buffer_index);
+			if (flag == 2 || flag == 4 || flag == 5)
+			{
+				_putchar(' ', buffer, buffer_index);
+				(*count)++;
+			}
+			_putchar(*format, buffer, buffer_index);
+			(*count) += 2;
+			break;
+	}
+}
+/**
+ * print_arg - A helper function to print args
+ * @format: A list of types of arguments passed to the function
+ * @args: The arguments to be printed
+ * @count: The number of characters printed
+ * @buffer: The buffer to store characters
+ * @buffer_index: The current index in the buffer
+ * @flag: contains the flags
+ */
+void print_arg(const char *format, int *count, va_list args, char *buffer,
+				int *buffer_index, int flag)
+{
+	int num;
+	
+	switch (*format)
+	{
+		case 'c':
+			_putchar(va_arg(args, int), buffer, buffer_index);
+			(*count)++;
+			break;
+		case 's':
+			check_str(va_arg(args, char *), count, buffer, buffer_index);
+			break;
+		case '%':
+			_putchar('%', buffer, buffer_index);
+			(*count)++;
+			break;
+		case 'd':
+		case 'i':
+			if (flag == 6)
+				print_long(va_arg(args, long), count, buffer, buffer_index);
+			else
+			{
+				num = va_arg(args, int);
+				print_number_flag(num, count, buffer, buffer_index, flag);
+				print_number(num, count, buffer, buffer_index, flag);
+			}
+			break;
+		case 'b':
+			print_binary(va_arg(args, unsigned int), count, buffer, buffer_index);
+			break;
+		case 'u':
+			if (flag == 6)
+				print_numlong(va_arg(args, unsigned long), count, buffer, buffer_index);
+			else if (flag == 7)
+				print_numshort(va_arg(args, unsigned int), count, buffer, buffer_index);
+			else
+				print_ui(va_arg(args, unsigned int), count, buffer, buffer_index, flag);
+			break;
+		default:
+			print_arg2(format, count, args, buffer, buffer_index, flag);
+	}
 }
 
 /**
- * print_buffer - Prints contents of buffer
- * @buffer: array of characters
- * @buff_ind: array length
+ * _printf - The program produces output according to a format
+ * @format: A character string, the format to follow.
+ *
+ * Return: The number of characters printed or -1 if failure
  */
-void print_buffer(char buffer[], int *buff_ind)
+int _printf(const char *format, ...)
 {
-	if (*buff_ind > 0)
+	int count = 0, flag = 0;
+	int buffer_index = 0;
+	char buffer[BUFFER_SIZE];
+	va_list args;
+	
+	if (format == NULL || (strlen(format) <= 2 && format[0] == '%' &&
+				(format[1] == '\0' || format[1] == ' ')))
+		return (-1);
+	
+	va_start(args, format);
+	
+	while (*format)
 	{
-		write(1, &buffer[0], *buff_ind);
+		if (*format != '%')
+		{
+			_putchar(*format, buffer, &buffer_index);
+			count++;
+		}
+		else
+		{
+			format++;
+			flag = check_flags(format);
+			if (flag)
+			{
+				if (flag == 4 || flag == 5)
+					format += 2;
+				else
+					format++;
+			}
+			
+			print_arg(format, &count, args, buffer, &buffer_index, flag);
+		}
+		format++;
 	}
-	*buff_ind = 0;
+	if (buffer_index > 0)
+		_write_buffer(buffer, &buffer_index);
+	
+	va_end(args);
+	
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,13 +1,45 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 
+/*Buffer*/
 #define BUFFER_SIZE 1024
 
-int _printf(const char *format, ...);
-void print_buffer(char buffer[], int *buff_ind);
+/*Print to Stdoutput*/
+void _putchar(char c, char *buffer, int *buffer_index);
+void _write_buffer(char *buffer, int *buffer_index);
 
-#endif /* MAIN_H */
+/*Printf function*/
+int _printf(const char *format, ...);
+
+/*Switch cases*/
+void print_arg(const char *format, int *count, va_list args, char *buffer,
+				int *buffer_index, int flag);
+void print_arg2(const char *format, int *count, va_list args, char *buffer,
+				int *buffer_index, int flag);
+
+/*Conversion specifier*/
+void check_str(char *str, int *count, char *buffer, int *buffer_index);
+int check_flags(const char *format);
+void print_number_flag(int n, int *count, char *buffer, int *buffer_index,
+				int flag);
+void print_number(int num, int *count, char *buffer, int *buffer_index,
+				int flag);
+void print_binary(unsigned int b, int *count, char *buffer, int *buffer_index);
+void print_ui(unsigned int u, int *count, char *buffer, int *buffer_index,
+				int flag);
+void print_octal(unsigned int o, int *count, char *buffer, int *buffer_index,
+				int flag);
+void print_hex(unsigned int h, int uppercase, int *count, char *buffer,
+				int *buffer_index, int flag);
+void print_str_nonChar(char *s, int *count, char *buffer, int *buffer_index);
+void print_ptr(void *p, int *count, char *buffer, int *buffer_index);
+void print_numlong(unsigned long n, int *count, char *buffer,
+				int *buffer_index);
+void print_numshort(unsigned short n, int *count, char *buffer,
+				int *buffer_index);
+void print_long(long n, int *count, char *buffer, int *buffer_index);
+
+#endif /*MAIN_H*/
